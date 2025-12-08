a294cfd611a7d0b910ed102943a6e865955125da
EthanDarbourne <edarbour@uwaterloo.ca>
2025-12-08 13:17:09 -0400

    day 8

---
+++ 2025/Day8/part2.py
@@ -0,0 +1,55 @@
+import heapq
+import math
+
+MAX_SIZE = 1000
+MAX_HEAP_SIZE = 10000
+
+with open('input.txt', 'r') as file:
+    lines = file.read().split('\n')
+
+    closestPairs = []
+    junctionBoxes = [tuple([int(x) for x in i.split(',')]) for i in lines]
+    n = len(junctionBoxes)
+
+    def ComputeDistanceTuple(box1, box2):
+        dist = 0
+        for i in range(len(box1)):
+            dist += pow(box1[i] - box2[i], 2)
+        return (-math.sqrt(dist), box1, box2)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            heapq.heappush(closestPairs, ComputeDistanceTuple(junctionBoxes[i], junctionBoxes[j]))
+            if len(closestPairs) > MAX_HEAP_SIZE:
+                heapq.heappop(closestPairs)
+
+    circuits = []
+    closestPairs = [(-x[0], x[1], x[2]) for x in closestPairs]
+    heapq.heapify(closestPairs)
+    
+    while len(closestPairs) > 0:
+        (distance, box1, box2) = heapq.heappop(closestPairs)
+        box1Index = -1
+        box2Index = -1
+        for i in range(len(circuits)):
+            if box1 in circuits[i]:
+                box1Index = i
+            if box2 in circuits[i]:
+                box2Index = i
+        
+        if box1Index == -1 and box2Index == -1:
+            circuits.append([box1,box2])
+        elif box1Index == box2Index:
+            continue
+        elif box1Index == -1:
+            circuits[box2Index].append(box1)
+        elif box2Index == -1:
+            circuits[box1Index].append(box2)
+        else:
+            circuits[box1Index].extend(circuits[box2Index])
+            circuits.pop(box2Index)
+        
+        longestCircuits = sorted([len(circuit) for circuit in circuits])
+        if longestCircuits[0] == MAX_SIZE:
+            print(box1[0] * box2[0], box1,box2)
+            break
